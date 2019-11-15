7d4abf5568d172dd18198befd321b4a094a2a184
alexchamorro0x <oriondigitalconsulting@gmail.com>
2019-11-14 22:55:44 -0500

    CreateAccount ComboBoxes populated
    All scenes set to 1000x600
    
    Working on project appearance

--- src/Resort/EditAccountScene/EditAccountController.java
+++ src/Resort/EditAccountScene/EditAccountController.java
@@ -1,67 +1,55 @@
 package Resort.EditAccountScene;
 
-import Resort.ManagerViewScene.ManagerViewController;
-import Resort.TitleScene.TitleController;
-import Resort.Utility.AccountInformation;
 import Resort.Utility.DatabaseAgent;
-import Resort.Utility.SessionInformation;
+import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.Glow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import Resort.ManagerViewScene.ManagerViewController;
+import Resort.TitleScene.TitleController;
+import Resort.Utility.AccountInformation;
+import Resort.Utility.SessionInformation;
+import javafx.scene.control.Button;
 
 public class EditAccountController {
 
-  @FXML
-  private AnchorPane btnCreateAccount;
-
-  @FXML
-  private TextField tfFirstName;
-
-  @FXML
-  private TextField tfLastName;
-
-  @FXML
-  private TextField tfUsername;
-
-  @FXML
-  private TextField tfEmail;
-
-  @FXML
-  private TextField tfPassword;
-
-  @FXML
-  private TextField tfConfirmPassword;
-
-  @FXML
-  private TextField tfAddress;
-
-  @FXML
-  private TextField tfState;
-
-  @FXML
-  private TextField tfzipcode;
-
-  @FXML
-  private TextField tfCreditCardNumber;
-
-  @FXML
-  private TextField tfCvv;
-
-  @FXML
-  private Label lblCreateIndicate;
-
-  @FXML
-  private Button btnReturnToManager;
+  @FXML private ComboBox expireMonth;
+  @FXML private ComboBox expireYear;
+  @FXML private ImageView homeLogo;
+  @FXML private Label lblCreateIndicate;
+  @FXML private TextField tfFirstName;
+  @FXML private TextField tfLastName;
+  @FXML private TextField tfUsername;
+  @FXML private TextField tfEmail;
+  @FXML private TextField tfPassword;
+  @FXML private TextField tfAddress;
+  @FXML private TextField tfState;
+  @FXML private TextField tfzipcode;
+  @FXML private TextField tfCreditCardNumber;
+  @FXML private TextField tfCvv;
+  @FXML private TextField tfConfirmPassword;
+  @FXML private Button btnReturnToManager;
+  ObservableList<String> expireMonthList =
+      FXCollections.observableArrayList(
+          "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
+  ObservableList<String> expireYearList =
+      FXCollections.observableArrayList(
+          "2019", "2020", "2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029",
+          "2030");
 
   // String to hold our username if logged in and if user is a manager
   private SessionInformation sessionInformation = new SessionInformation();
@@ -69,9 +57,19 @@ public class EditAccountController {
   private String userNameForManagerEdit;
 
   public void initialize() {
-    // For users editing their own account managerView button will be disabled
+    expireMonth.setItems(expireMonthList);
+    expireYear.setItems(expireYearList);
+
+    lblCreateIndicate.setVisible(false);
     btnReturnToManager.setVisible(false);
+
+    File RoomA = new File("src/Resort/RoomFinderScene/pineapple.png");
+    Image pineapple = new Image(RoomA.toURI().toString());
+    homeLogo.setImage(pineapple);
+    homeLogo.setFitWidth(65);
+    homeLogo.setFitHeight(100);
   }
+
   // Setter so that when called from manager view to edit another users account this can be set
   public void setUserNameForManagerEdit(String userNameForManagerEdit) {
     this.userNameForManagerEdit = userNameForManagerEdit;
@@ -90,7 +88,6 @@ public class EditAccountController {
     tfPassword.setText(userAccount.getPassWord());
     tfConfirmPassword.setText(userAccount.getPassWord());
     tfCvv.setText(userAccount.getCvv());
-
   }
 
   // setter for session information
@@ -106,13 +103,13 @@ public class EditAccountController {
 
   @FXML
   void btnClickUpdateAccount(MouseEvent event) {
-    //todo add logic to update the account in the database
+    // todo add logic to update the account in the database
   }
 
   @FXML
   void btnClickHome(MouseEvent event) throws IOException {
-    //get a reference to the window we are in
-    Stage window = (Stage) btnCreateAccount.getScene().getWindow();
+    // get a reference to the window we are in
+    Stage window = (Stage) tfUsername.getScene().getWindow();
 
     // declare and initialize a loader for the FXML scene we are going to
     FXMLLoader loader = new FXMLLoader();
@@ -131,11 +128,32 @@ public class EditAccountController {
     window.setScene(titleScene);
   }
 
+  public void btnHomeEntered(MouseEvent mouseEvent) {
+    homeLogo.setFitHeight(70);
+    homeLogo.setFitHeight(105);
+
+    Glow glow = new Glow();
+    glow.setLevel(.15);
+    homeLogo.setEffect(glow);
+
+    Resort.RoomFinderScene.RoomFinderController.pictureBorder(homeLogo);
+  }
+
+  public void btnHomeExited(MouseEvent mouseEvent) {
+    homeLogo.setFitHeight(65);
+    homeLogo.setFitHeight(100);
+    File RoomA = new File("src/Resort/RoomFinderScene/pineapple.png");
+    Image pineapple = new Image(RoomA.toURI().toString());
+    homeLogo.setImage(pineapple);
+  }
+
   public void setSessioninformation(SessionInformation sessionformation) {
     this.sessionInformation = sessionformation;
 
-    // If this is called from the manager view to edit another users information set info for that user
-    AccountInformation userAccount = DatabaseAgent.getAccountInformation(sessionformation.getUserName());
+    // If this is called from the manager view to edit another users information set info for that
+    // user
+    AccountInformation userAccount =
+        DatabaseAgent.getAccountInformation(sessionformation.getUserName());
 
     System.out.println("Test");
 
@@ -153,7 +171,7 @@ public class EditAccountController {
 
   @FXML
   void clickReturnToManager(ActionEvent event) throws IOException {
-    Stage window = (Stage)  tfUsername.getScene().getWindow();
+    Stage window = (Stage) tfUsername.getScene().getWindow();
 
     // declare and initialize a loader for the FXML scene we are going to
     FXMLLoader loader = new FXMLLoader();
@@ -166,7 +184,6 @@ public class EditAccountController {
     ManagerViewController managerViewController = loader.getController();
 
     managerViewController.setSessionInformation(sessionInformation);
-
 
     // make the new scene we are going to
     Scene managerScene = new Scene(managerView);
@@ -197,5 +214,18 @@ public class EditAccountController {
 
     DatabaseAgent.updateAccount(accountInformation);
   }
+
+  /*
+   public CreateAccountController(Stage CreateAccount) throws IOException {
+
+     Parent root = FXMLLoader.load(getClass().getResource("CreateAccountScene/CreateAccount.fxml"));
+     CreateAccount.setTitle("Resort Reservations");
+     root.getStylesheets().add
+             (Main.class.getResource("resortTemplate.css").toExternalForm());
+     CreateAccount.setScene(new Scene(root, 850, 530));
+     CreateAccount.show();
+   }
+
+  */
 
 }
